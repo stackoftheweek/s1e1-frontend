f75e4e8f8fdd415a6b863ea5b2527ec8bdebb8c8
Will Knowles <will@willknowles.com>
2024-12-13 09:40:31 -0600

    Removed some unused code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
 import { Chat } from './components/chat'
 </script>
 
@@ -8,16 +7,5 @@ import { Chat } from './components/chat'
 </template>
 
 <style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+
 </style>
